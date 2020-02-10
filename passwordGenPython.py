f53493d15b6b77a26ce15284ae418b0c9e17554f
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-02-10 15:52:02 -0800

    Update passwordGenPython.py

--- passwordGenPython.py
+++ passwordGenPython.py
@@ -1,23 +1,20 @@
 import random
 
-characters= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-          'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k',
-          'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z',
-          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'M', 'N', 'O',
-          'p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','@', '#', '$',
-          '%', '=', ':', '?', '.', '/', '|', '~', '>','*', '(', ')']
+characters= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k',
+          'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+          'I', 'J', 'K', 'M', 'N', 'O','p', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','@', '#', '$','%', '=', 
+          ':', '?', '.', '/', '|', '~', '>','*', '(', ')']
 
-char1 = characters[random.randint(0,75)]
-char2 = characters[random.randint(0,75)]
-char3 = characters[random.randint(0,75)]
-char4 = characters[random.randint(0,75)]
-char5 = characters[random.randint(0,75)]
-char6 = characters[random.randint(0,75)]
-char7 = characters[random.randint(0,75)]
-char8 = characters[random.randint(0,75)]
-char9 = characters[random.randint(0,75)]
-char10 = characters[random.randint(0,75)]
+char1 = characters[random.randint(0,74)]
+char2 = characters[random.randint(0,74)]
+char3 = characters[random.randint(0,74)]
+char4 = characters[random.randint(0,74)]
+char5 = characters[random.randint(0,74)]
+char6 = characters[random.randint(0,74)]
+char7 = characters[random.randint(0,74)]
+char8 = characters[random.randint(0,74)]
+char9 = characters[random.randint(0,74)]
+char10 = characters[random.randint(0,74)]
 
 
-print(str(char1)+str(char2)+str(char3)+str(char4)+str(char5)+str(char6)
-      +str(char7)+str(char8)+str(char9)+str(char10))
+print(str(char1)+str(char2)+str(char3)+str(char4)+str(char5)+str(char6)+str(char7)+str(char8)+str(char9)+str(char10))
